5ff4d773e680b9716a45e4f45b231e39add75ab5
mazebeat <dfeliu@intelidata.cl>
2017-04-10 16:55:30 -0300

    v0.0.1

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-
-import Title from './Header/Title'
+import React from 'react';
+import Title from './Header/Title';
 
 export default class Header extends React.Component {
     handleChange(e) {
